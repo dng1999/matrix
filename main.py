81a23827d13c938ea32d927a9e75397632faf20e
yureistar <dng1@stuy.edu>
2017-02-28 11:52:42 -0500

    matrix_mult still needs work

--- main.py
+++ main.py
@@ -4,6 +4,37 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
+print("Printing matrix.")
+print_matrix(matrix)
+
+print("Running ident(matrix)...")
+matrix = ident(matrix)
+print("Printing result.")
+print_matrix(matrix)
+
+print("Running scalar_mult(matrix,2)...")
+matrix = scalar_mult(matrix,2)
+print("Printing result.")
+print_matrix(matrix)
+
+print("Making matrix m1 with (1,2,3)(4,5,6)...")
+m1 = []
+m1.append([1,2,3])
+m1.append([4,5,6])
+print("Printing m1.")
+print_matrix(m1)
+
+print("Making matrix m1 with (7,8)(9,10)(11,12)...")
+m2 = []
+m2.append([7,8])
+m2.append([9,10])
+m2.append([11,12])
+print("Printing m2.")
+print_matrix(m2)
+
+print("Running matrix_mult(m1,m2)...")
+matrix = matrix_mult(m1,m2)
+print("Printing result.")
 print_matrix(matrix)
 
 #draw_lines( matrix, screen, color )
--- matrix.py
+++ matrix.py
@@ -2,21 +2,37 @@ import math
 
 
 def print_matrix( matrix ):
-    for r in range(0,4):
-        print("%d %d %d %d" % (matrix[0][r],matrix[1][r],matrix[2][r],matrix[3][r]))
+    for r in range(len(matrix[0])):
+        for c in range(len(matrix)):
+            print matrix[c][r],
+        print
             
 def ident( matrix ):
-    pass
+    m = []
+    m.append([1,0,0,0])
+    m.append([0,1,0,0])
+    m.append([0,0,1,0])
+    m.append([0,0,0,1])
+    return m
 
 def scalar_mult( matrix, s ):
-    for c in range(0,4):
-        for r in range(0,4):
+    for c in range(len(matrix)):
+        for r in range(len(matrix[0])):
             matrix[c][r] *= s
+    return matrix
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
-
+    m = []
+    for c1 in range(len(m1)):
+        m.append([])
+        for r2 in range(len(m2[0])):
+            res = 0
+            for r1 in range(len(m1[0])):
+                res += (m1[c1][r1]*m2[r2][c1])
+            m[c1].append(res)
+    return m
+                
 def new_matrix(rows = 4, cols = 4):
     m = []
     for c in range( cols ):
